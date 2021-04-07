69db1edb6b3007f4bbf0da44a35129bcdeecc33d
TbotaPhantA <botakiller@mail.ru>
2021-04-08 01:16:40 +0300

    welcome block basically done

--- src/components/WelcomePage/WelcomeBlock/WelcomeBlock.js
+++ src/components/WelcomePage/WelcomeBlock/WelcomeBlock.js
@@ -4,7 +4,7 @@ import './WelcomeBlock.css';
 function WelcomeBlock() {
     return (
         <div className="welcome_block">
-            <h1 className="welcome_block_title">Welcome to True Rap lair</h1>
+            <h1 className="welcome_block_title">Welcome to <span>True Rap</span> lair</h1>
             <p className="welcome_block_text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo sit quas neque excepturi maiores molestias illo non labore commodi fugit!</p>
             <div className="welcome_block_sign_up_btn">Become The True Rapper</div>
             <div className="welcome_block_sign_in_btn">Enter your account</div>
