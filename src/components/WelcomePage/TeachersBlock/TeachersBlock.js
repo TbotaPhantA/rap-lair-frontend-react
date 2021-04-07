7dee1f79e8a9c567ec433b37ff0d654e398e945c
TbotaPhantA <botakiller@mail.ru>
2021-04-08 02:13:45 +0300

    modified teachers block, but not finished

--- src/components/WelcomePage/TeachersBlock/TeachersBlock.js
+++ src/components/WelcomePage/TeachersBlock/TeachersBlock.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './TeachersBlock.css';
 
 function TeachersBlock() {
     return (
-        <div>
-            
+        <div className="teachers_block">
+            <div className="teachers_block_title">
+                <div className="teachers_block_title_fill">
+                    <span>Our teachers:</span>
+                </div>
+            </div>
+            <div className="teachers_block_carousel">
+                funny carousel
+            </div>
         </div>
     );
 }
