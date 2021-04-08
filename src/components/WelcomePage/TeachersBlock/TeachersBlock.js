1173ba4c6cb4bd167be43031db4abeca3f807dfc
TbotaPhantA <botakiller@mail.ru>
2021-04-09 01:52:00 +0300

    little changes were added such as forbiting selection of text

--- src/components/WelcomePage/TeachersBlock/TeachersBlock.js
+++ src/components/WelcomePage/TeachersBlock/TeachersBlock.js
@@ -5,23 +5,23 @@ function TeachersBlock() {
     return (
         <div className="teachers_block">
             <div className="teachers_block_title">
-                <div className="teachers_block_title_fill">
+                <div className="teachers_block_title_fill unselectable_text">
                     <span>Our teachers:</span>
                 </div>
             </div>
             <div className="teachers_block_carousel">
                 <div className="carousel_track">
                         <div className="carousel_element">
-                            <span className="name_of_teacher">LIL TOKMISTIK228</span>
-                            <span className="description_of_teacher">Lorem ipsum dolor sit amet, consectetur adipisicing.</span>
+                            <span className="name_of_teacher unselectable_text">LIL TOKMISTIK228</span>
+                            <span className="description_of_teacher unselectable_text">Lorem ipsum dolor sit amet, consectetur adipisicing.</span>
                         </div>
                         <div className="carousel_element">
-                            <span className="name_of_teacher">GERMAN</span>
-                            <span className="description_of_teacher">Lorem ipsum dolor sit amet consectetur.</span>
+                            <span className="name_of_teacher unselectable_text">GERMAN</span>
+                            <span className="description_of_teacher unselectable_text">Lorem ipsum dolor sit amet consectetur.</span>
                         </div>
                         <div className="carousel_element">
-                            <span className="name_of_teacher">mc.bread</span>
-                            <span className="description_of_teacher">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</span>
+                            <span className="name_of_teacher unselectable_text">mc.bread</span>
+                            <span className="description_of_teacher unselectable_text">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</span>
                         </div>
                 </div>
             </div>
--- src/components/WelcomePage/WelcomeBlock/WelcomeBlock.js
+++ src/components/WelcomePage/WelcomeBlock/WelcomeBlock.js
@@ -4,10 +4,10 @@ import './WelcomeBlock.css';
 function WelcomeBlock() {
     return (
         <div className="welcome_block">
-            <h1 className="welcome_block_title">Welcome to <span>True Rap</span> lair</h1>
-            <p className="welcome_block_text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo sit quas neque excepturi maiores molestias illo non labore commodi fugit!</p>
-            <div className="welcome_block_sign_up_btn">Become The True Rapper</div>
-            <div className="welcome_block_sign_in_btn">Enter your account</div>
+            <h1 className="welcome_block_title unselectable_text">Welcome to <span>True Rap</span> lair</h1>
+            <p className="welcome_block_text unselectable_text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo sit quas neque excepturi maiores molestias illo non labore commodi fugit!</p>
+            <div className="welcome_block_sign_up_btn unselectable_text">Become The True Rapper</div>
+            <div className="welcome_block_sign_in_btn unselectable_text">Enter your account</div>
             <div className="welcome_block_german_field">
                 <div className="welcome_block_german"></div>
             </div>
