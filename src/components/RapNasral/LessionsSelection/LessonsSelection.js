9e1f9dde659a132e2d1251efda9ddd002758a5b0
TbotaPhantA <botakiller@mail.ru>
2021-04-12 03:19:42 +0300

    lessons_selection page basically done

--- src/components/RapNasral/LessionsSelection/LessonsSelection.js
+++ src/components/RapNasral/LessionsSelection/LessonsSelection.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import './LessonsSelection.css';
 import DN from '../../../images/dancing_niggers.gif';
+import {Link} from 'react-router-dom';
 
 function LessonsSelection() {
     return (
         <div className="lessons_selection">
             <img className="dancing_niggers" src={DN} alt=""/>
+            <h1 className="unselectable_text">Which Articles do you want to read?</h1>
+            <div className="LS_buttons_wrapper">
+                <Link className="LS_button unselectable_text" to="/lessons_selection" >
+                    <span></span>                
+                    <span></span>                
+                    <span></span>                
+                    <span></span>                
+                    Teachers Articles
+                </Link>
+
+                <Link className="LS_button unselectable_text" to="/lessons_selection" >
+                    <span></span>                
+                    <span></span>                
+                    <span></span>                
+                    <span></span>                
+                    Community Articles
+                </Link>
+            </div>
         </div>
     );
 }
