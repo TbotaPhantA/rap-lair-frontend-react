a81566ee24be589d83864fe948c55893b3bb6975
TbotaPhantA <botakiller@mail.ru>
2021-04-16 13:48:41 +0300

    font size changed from rem to vw

---
+++ src/components/RapNasral/Profile/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Profile.css';
+
+function profile() {
+    return (
+        <div className="profile_wrapper">
+            I'm the profile
+        </div>
+    );
+}
+
+export default profile;
--- src/components/RapNasral/RapNasral.js
+++ src/components/RapNasral/RapNasral.js
@@ -4,30 +4,31 @@ import LessonsSelection from './LessionsSelection/LessonsSelection';
 import {BrowserRouter as Router, Redirect, Route, Switch} from 'react-router-dom';
 import CommunityArticles from './CommunityArticles/CommunityArticles';
 import CArticle from './CommunityArticles/CArticle/CArticle';
+import Profile from './Profile/Profile';
 
 function RapNasral() {
     return (
         <div className="rap_nasral">
-                <Switch>
-                    <Route path="/profile/:user_id">
-                        I'm the profile
-                    </Route>
-                    <Route path="/lessons_selection">
-                        <LessonsSelection />
-                    </Route>
-                    <Route path="/teachers_articles">
-                        {/* <TeachersArticles /> */}
-                    </Route>
-                    <Route path="/community_articles/:CA_id">
-                        <CArticle />
-                    </Route>
-                    <Route path="/community_articles">
-                        <CommunityArticles />
-                    </Route>
-                    <Route path='/'>
-                        <Redirect to="/lessons_selection" />
-                    </Route>
-                </Switch>
+            <Switch>
+                <Route path="/profile/:user_id">
+                    <Profile />
+                </Route>
+                <Route path="/lessons_selection">
+                    <LessonsSelection />
+                </Route>
+                <Route path="/teachers_articles">
+                    {/* <TeachersArticles /> */}
+                </Route>
+                <Route path="/community_articles/:CA_id">
+                    <CArticle />
+                </Route>
+                <Route path="/community_articles">
+                    <CommunityArticles />
+                </Route>
+                <Route path='/'>
+                    <Redirect to="/lessons_selection" />
+                </Route>
+            </Switch>
         </div>
     );
 }
